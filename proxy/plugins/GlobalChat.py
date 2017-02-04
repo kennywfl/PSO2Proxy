8bb099dfe3c98adc715a8b73225d1136fa625161
kennywfl <hikari0258@gmail.com>
2017-02-04 16:23:12 +0800

    Add requirement while install
    Update formatting

--- proxy/plugins/GlobalChat.py
+++ proxy/plugins/GlobalChat.py
@@ -60,11 +60,11 @@ def onReceiveTelegramChat(bot, update):
                 client.get_handle().send_crypto_packet(
                     packetFactory.TeamChatPacket(update.message.from_user.id,
                                                  "[Telegram] %s" % update.message.from_user.username,
-                                                 "[Telegram] %s" % update.message.from_user.username, "* %s%s" % (
+                                                 "[Telegram] %s" % update.message.from_user.username, "%s%s" % (
                                                      client.preferences.get_preference('globalChatPrefix'),
                                                      msg.decode('utf-8', 'ignore'))).build())
             else:
-                client.get_handle().send_crypto_packet(packetFactory.SystemMessagePacket("[Telegram] <%s> * %s" % (
+                client.get_handle().send_crypto_packet(packetFactory.SystemMessagePacket("[Telegram] <%s> %s" % (
                     update.message.from_user.username, "%s%s" % (client.preferences.get_preference('globalChatPrefix'),
                                                                  msg.decode('utf-8', 'ignore'))), 0x3).build())
 
